30a378457f5c9bc9ae21d8134f97242148c25fff
Tony <tony.demol@gmail.com>
2015-12-07 22:08:32 +0100

    Add REST client

--- rest-gs/rest-gs-client/src/main/java/com/tony/sandbox/rest/gs/client/UserServiceRestClient.java
+++ rest-gs/rest-gs-client/src/main/java/com/tony/sandbox/rest/gs/client/UserServiceRestClient.java
@@ -1,5 +1,23 @@
 package com.tony.sandbox.rest.gs.client;
 
+import java.net.URI;
+
+import javax.ws.rs.client.Client;
+import javax.ws.rs.client.ClientBuilder;
+import javax.ws.rs.client.WebTarget;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+
 public class UserServiceRestClient {
 
+	public static void main(final String[] args) {
+		final Client client = ClientBuilder.newClient();
+		final WebTarget webTarget = client.target(URI.create("http://localhost:8080/rest-gs-server-servlet3-withwebxml/rest/UserService")).path("json").path("0");
+		System.out.println("Target URI : " + webTarget.getUri());
+		final Response response = webTarget.request().accept(MediaType.APPLICATION_JSON).get();
+		System.out.println("Response status : " + response.getStatus());
+		System.out.println("Response header : " + response.getHeaders());
+		// System.out.println("Payload : " + response.readEntity(String.class));
+		System.out.println("Payload : " + response.readEntity(User.class));
+	}
 }
